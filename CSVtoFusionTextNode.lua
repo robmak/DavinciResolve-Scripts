2e25fbb4aa88f56e00a2a45e40ce9d6318f5f870
robmak <robmak@users.noreply.github.com>
2021-02-03 17:33:08 +0100

    Add heightValue evaluation to CSV2FusionTextNode
    Remove line number

--- CSVtoFusionTextNode.lua
+++ CSVtoFusionTextNode.lua
@@ -3,8 +3,8 @@ Taking values (e.g. speed info) from CSV file for every frame of a clip
 and put it into a Fusion Text+ node.
 
 CSV is parsed on "Start Render Scripts" and Text+ StyledText property is
-replaced on Frame Render Scripts. Every line of CSV file contains frame number and speed
-value separated by ; (e.g. 1;42). This CSV file must be named like the current clip file
+replaced on Frame Render Scripts. Every line of CSV file contains speed value and height
+value separated by ; (e.g. 23;42). This CSV file must be named like the current clip file
 and path just with a different file extension (.csv instead of .mp4) and must be placed
 in the same folder.
 ]]
@@ -13,18 +13,21 @@ in the same folder.
 
 function readCSV(path)
 	io.input(path)
-	local inputTable = {}
+	local speedTable = {}
+	local heightTable = {}
 	for line in io.lines() do
-		sepPos = string.find(line, ";") + 1
-		speedValue = string.sub(line, sepPos)
-		table.insert(inputTable, speedValue)
+		sepPos = string.find(line, ";")
+		speedValue = string.sub(line, 1, sepPos)
+		heigthValue = string.sub(line, sepPos + 1)
+		table.insert(speedTable, speedValue)
+		table.insert(heightTable, heightValue)
 	end
 	io.close()
-	return inputTable
+	return speedTable, heightTable
 end
 
 oPath = MediaIn1:GetData('MediaProps').MEDIA_PATH
-timeTable = readCSV(string.sub(oPath, 1, string.len(oPath) - 3) .. "csv")
+speedTable, heightTable = readCSV(string.sub(oPath, 1, string.len(oPath) - 3) .. "csv")
 Text1:SetData("cpath", timeTable)
 
 
